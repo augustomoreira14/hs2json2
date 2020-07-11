ce135cee35c16edde253ef9a022c9587f9b6b3b5
Augusto Moreira <augustomoreira14@gmail.com>
2020-07-11 14:19:18 -0300

    Refatora a função qsort

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,7 +6,7 @@ module Lib
 
 qsort :: Ord a => [a] -> [a]
 qsort []     = []
-qsort (x:xs) = qsort lhs ++ [x] ++ qsort lhs
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
     where lhs = filter  (< x) xs
           rhs = filter (>= x) xs
 

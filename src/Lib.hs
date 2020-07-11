4681d3f42fb21d87d67ca13e753b7a3dc56b6778
Augusto Moreira <augustomoreira14@gmail.com>
2020-07-11 14:14:38 -0300

    Adiciona função qsort

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
 module Lib
-    ( someFunc
+    ( 
+        someFunc,
+        qsort
     ) where
+
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort lhs
+    where lhs = filter  (< x) xs
+          rhs = filter (>= x) xs
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
